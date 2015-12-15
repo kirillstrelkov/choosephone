4956f1df6a28cf7d124791d6f1c1f5658bfd7ff1
Kirill Strelkov <kirill.strelkov@helpling.com>
2015-12-15 16:57:16 +0100

    added SSL for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,4 +74,5 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.force_ssl = true
 end
