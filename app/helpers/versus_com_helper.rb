2b1684a416548ea78776855c356da4721fa21e56
Kirill Strelkov <kirillstrelkov@users.noreply.github.com>
2018-08-06 21:59:20 +0300

    fixed top phone url

--- app/helpers/versus_com_helper.rb
+++ app/helpers/versus_com_helper.rb
@@ -9,7 +9,7 @@ module VersusComHelper
   include Capybara::DSL
 
   VERSUS_URL = 'https://www.versus.com'.freeze
-  VERSUS_URL_WITH_TO_PHONE = 'https://versus.com/en/sony-xperia-z5-premium-dual-vs-'.freeze
+  VERSUS_URL_WITH_TO_PHONE = 'https://versus.com/en/samsung-galaxy-s9-plus-qualcomm-snapdragon-845-vs-'.freeze
   AMAZON_SEARCH_URL = 'https://versus.com/pricetags/get'.freeze
 
   def get_price(phone_name)
