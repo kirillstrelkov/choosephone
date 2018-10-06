2b20e1a8ab7ac5ede288c0f6fd9ba648a563ab40
Kirill Strelkov <kirillstrelkov@users.noreply.github.com>
2018-10-06 12:35:34 +0200

    fixed period in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  get 'versuscom/:phone_name/price' => 'versus_com#price', defaults: { format: 'json' }
+  get 'versuscom/:phone_name/price' => 'versus_com#price', defaults: { format: 'json' }, constraints: { phone_name: /.*/ }
 
   scope ':locale', locale: /en|ru/ do
     get '' => 'phones#index'
     get 'index' => 'phones#index'
     get 'phones/index'
     get 'phones/compare'
-    get 'versuscom/:phone_name/points' => 'versus_com#points', defaults: { format: 'json' }
+    get 'versuscom/:phone_name/points' => 'versus_com#points', defaults: { format: 'json' }, constraints: { phone_name: /.*/ }
   end
 
   root to: redirect("/#{I18n.default_locale}")
